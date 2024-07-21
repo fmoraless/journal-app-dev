07b9baed28595bb27158118e696de5fa33a2d3da
fmoraless <franticzero@gmail.com>
2024-07-21 15:38:19 -0400

    feat: Add "Add Note" button to JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,18 +1,35 @@
 import React from "react";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelected } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelected /> */}
+      {/* ===== Nothing Selected ====== */}
+      <NothingSelected />
 
-      {/* Nothing Selected */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": {
+            backgroundColor: "error.main",
+            opacity: 0.9,
+          },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
 
-      {/* Note View */}
+      {/* ======= Note View ======== */}
 
-      <NoteView />
+      {/* <NoteView /> */}
     </JournalLayout>
   );
 };
